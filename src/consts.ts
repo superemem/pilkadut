0e3174f191db124d43902e4f4f659c3c35eaaf29
superemem <m9irfan@gmail.com>
2024-10-15 01:45:49 +0700

    mengubah beberapa tapilan navbar, menambah icon pada site title, mengecilkan header di home

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Pilkada Sumbar';
-export const SITE_DESCRIPTION = 'Informasi Pemilihan Kepala Daerah Sumatera Barat 2024';
+export const SITE_TITLE = "Pilkada Sumbar";
+export const SITE_DESCRIPTION =
+  "Informasi Pemilihan Kepala Daerah Sumatera Barat 2024";
