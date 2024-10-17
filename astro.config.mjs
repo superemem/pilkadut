0d089d7b55e83621094433fe02c5d7816ba6bacb
superemem <m9irfan@gmail.com>
2024-10-18 01:31:37 +0700

    enable analytics vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,12 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
-import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
-  adapter: vercel(),
-  webAnalytics: { enabled: true },
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
